fa7a26f6273caca80ff1d62938e5b9931390eaf3
EliezerDev <ardhiputrapradana21@gmail.com>
2021-03-19 20:43:55 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// hero images change background img
+const headerHero = document.querySelector('header');
+const imgList = ['bg1', 'bg2', 'bg3'];
+let selectImg = 0;
+setInterval(() => {
+  selectImg >= imgList.length ? selectImg = 0 : selectImg;
+  headerHero.style.backgroundImage = `url(./img/${imgList[selectImg]}.jpg)`;
+  selectImg++
+}, 5000);
+
+// scroll to up
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= document.getElementById('tentang').scrollHeight) {
+    document.querySelector('.scroll-to-up').classList.add('show-btn');
+  } else {
+    document.querySelector('.scroll-to-up').classList.remove('show-btn');
+  }
+});
